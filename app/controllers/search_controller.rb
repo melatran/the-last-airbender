5219868ef83c0b7531936f9587349f3092df34a4
Melanie Tran <melaniexx02@gmail.com>
2020-07-14 10:36:10 -0600

    Format the view page to display information

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,7 +4,6 @@ class SearchController < ApplicationController
     conn = Faraday.new("https://last-airbender-api.herokuapp.com/")
     response = conn.get("/api/v1/characters?affiliation=fire nation")
     @members = JSON.parse(response.body, symbolize_names: true)
-
     # nation = params[:element_nation]
     # conn = Faraday.new("https://last-airbender-api.herokuapp.com/")
     # response = conn.get("/api/v1/characters?affiliation=$#{nation}")
