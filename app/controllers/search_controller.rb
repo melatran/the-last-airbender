2f01f8ea0cfdbdc4ba8917330499a2ac7b7afa77
Melanie Tran <melaniexx02@gmail.com>
2020-07-14 10:24:19 -0600

    Can't get nation to pass without specifics

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,10 +1,19 @@
 class SearchController < ApplicationController
   def index
-    search_results = SearchResults.new
-    @members = search_results.members(nation)
+    element = params[:nation]
+    conn = Faraday.new("https://last-airbender-api.herokuapp.com/")
+    response = conn.get("/api/v1/characters?affiliation=fire nation")
+    @members = JSON.parse(response.body, symbolize_names: true)
+
     # nation = params[:element_nation]
     # conn = Faraday.new("https://last-airbender-api.herokuapp.com/")
     # response = conn.get("/api/v1/characters?affiliation=$#{nation}")
     # json = JSON.parse(response.body, symbolize_names: true)
+  end
+
+  private
+
+  def member_params
+    params.permit(:name, :nation)
   end
 end
--- app/services/last_airbender_service.rb
+++ app/services/last_airbender_service.rb
@@ -1,8 +1,11 @@
 class LastAirbenderService
 
-  def members_of_nation(element)
+  def members_of_nation
     conn = Faraday.new("https://last-airbender-api.herokuapp.com/")
-    response = conn.get("/api/v1/characters?affiliation=$#{element}")
+    response = conn.get("/api/v1/characters?affiliation=fire nation")
     json = JSON.parse(response.body, symbolize_names: true)
+
   end
 end
+
+# https://last-airbender-api.herokuapp.com/api/v1/characters?affiliation=fire nation
